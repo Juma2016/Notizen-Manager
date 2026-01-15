376e3dc41a6be9ef7b2fc7df2f2aa5e3719a8d53
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2026-01-15 14:42:17 +0100

    feat: use json response in express and post method for possible future use

--- backend/notebooks.js
+++ backend/notebooks.js
@@ -7,25 +7,72 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = 3000;
+const route = "/api/notebooks";
+const healthmessage = `Server running on PORT ${PORT}`;
+const filePath = path.join(__dirname, "notebooks.json");
 
 const app = express();
+app.use(express.json());
 
-app.get("/api/notebooks", async (_, res) => {
-  console.log(__dirname);
-  const filePath = path.join(__dirname, "notebooks.json");
+app.get(route, (_, res) => {
+  try {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        return res.status(500).send("Error reading notebooks.json file");
+      }
 
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      return res.status(500).send("Error reading notebooks.json file");
+      const notebooks = JSON.parse(data);
+      return res.status(200).send(notebooks);
+    });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .send({ error: error, message: "Internal Server Error" });
+  }
+});
+
+/**
+ * Implemented post for possible future use
+ */
+app.post(route, (req, res) => {
+  try {
+    const { id, title } = req.body;
+    if (!id || !title) {
+      return res.status(400).send("Required Fields missing");
     }
 
-    const notebooks = JSON.parse(data);
-    res.status(200).send(notebooks);
-  });
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        return res.status(500).send("Error reading notebooks.json file");
+      }
+
+      const oldNoteBooks = JSON.parse(data);
+      const jsonData = JSON.stringify([
+        ...oldNoteBooks,
+        { id: id, title: title },
+      ]);
+
+      fs.writeFile(filePath, jsonData, (err) => {
+        if (err) {
+          return res.status(500).send("Error posting new notebook");
+        } else {
+          return res
+            .status(200)
+            .send({ id, title, message: "Succeesfully appended new Notebook" });
+        }
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .send({ error: error, message: "Internal Server Error" });
+  }
 });
 
 app.get("/", async (_, res) => {
-  return res.send(`Server running on PORT ${PORT}`);
+  return res.send(healthmessage);
 });
 
-app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+app.listen(PORT, () => console.log(healthmessage));
